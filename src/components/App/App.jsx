8bec009a300bfb0643fb2d570a51f166c9737c06
Fatiuk <125760945+Fatiuk@users.noreply.github.com>
2023-09-17 17:14:07 +0300

    Update input

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -5,10 +5,18 @@ import Section from '../Section/Section';
 import Searchbar from '../Searchbar/Searchbar';
 
 export default class App extends Component {
+  state = {
+    query: '',
+  };
+  // Function that sets the state with the dataForm value upon submission.
+  handleSubmit = dataForm => {
+    this.setState({ query: dataForm.trim() });
+  };
+
   render() {
     return (
       <>
-        <Searchbar></Searchbar>
+        <Searchbar onSubmit={this.handleSubmit}></Searchbar>
         <Section></Section>
       </>
     );
--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -8,21 +8,39 @@ import {
 } from './Searchbar.styled';
 
 export default class Searchbar extends Component {
+  state = {
+    query: '',
+  };
+  // Function that updates the state based on user input
+  handleChange = event => {
+    const inputValue = event.target.value;
+    this.setState({ query: inputValue });
+  };
+  // Function that passes the state to the App.jsx
+  handleSubmit = event => {
+    event.preventDefault();
+    if (!this.state.query.trim()) {
+      return console.log(111);
+    }
+    this.props.onSubmit(this.state.query);
+  };
+
   render() {
     return (
-      <SearchbarWrap className="searchbar">
-        <SearchbarForm className="form">
-          <SearchbarButton type="submit" className="button">
-            <SearchbarSpan className="button-label"></SearchbarSpan>
+      <SearchbarWrap>
+        <SearchbarForm onSubmit={this.handleSubmit}>
+          <SearchbarButton type="submit">
+            <SearchbarSpan></SearchbarSpan>
           </SearchbarButton>
 
           <SearchbarInput
             id="searchInput"
-            className="input"
             type="text"
             autoComplete="off"
             autoFocus
             placeholder="Search images and photos"
+            value={this.state.query}
+            onChange={this.handleChange}
           />
         </SearchbarForm>
       </SearchbarWrap>
