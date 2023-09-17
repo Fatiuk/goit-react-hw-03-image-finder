c4f90f23f513d5a3b0254923da4e23218277919b
Fatiuk <125760945+Fatiuk@users.noreply.github.com>
2023-09-17 16:24:26 +0300

    Fix className

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -10,18 +10,18 @@ import {
 export default class Searchbar extends Component {
   render() {
     return (
-      <SearchbarWrap class="searchbar">
-        <SearchbarForm class="form">
-          <SearchbarButton type="submit" class="button">
-            <SearchbarSpan class="button-label"></SearchbarSpan>
+      <SearchbarWrap className="searchbar">
+        <SearchbarForm className="form">
+          <SearchbarButton type="submit" className="button">
+            <SearchbarSpan className="button-label"></SearchbarSpan>
           </SearchbarButton>
 
           <SearchbarInput
             id="searchInput"
-            class="input"
+            className="input"
             type="text"
-            autocomplete="off"
-            autofocus
+            autoComplete="off"
+            autoFocus
             placeholder="Search images and photos"
           />
         </SearchbarForm>
